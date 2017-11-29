beb10a1413e733d8a4f31ccbd9e6ccf181a53669
Carl Camit <carlcamit0@gmail.com>
2017-11-29 12:02:53 +1100

    added a way to edit first name on profile

--- src/App.js
+++ src/App.js
@@ -10,6 +10,21 @@ class App extends Component {
     }
   }
 
+  handleChangeFirstName = (event) => {
+     // Get the <input> (DOM element)
+     const input = event.target
+     // Get the current input text from the field
+     const newFirstName = input.value
+     this.setState((prevState) => {
+       const user = prevState.user
+       user.firstName = newFirstName
+       return {
+         // this.state.user will be updated
+         user: user
+       }
+     })
+  }
+
   render() {
     const user = this.state.user
 
@@ -18,6 +33,16 @@ class App extends Component {
         <h1>LinkedIn Profile Editor</h1>
         <img src={ user.profileImageURL } alt={ user.firstName + ' ' + user.lastName }/>
         <p>Name: { user.firstName } { user.lastName } </p>
+
+        <label>
+          First Name: { ' ' }
+          <input 
+            value={ user.firstName } 
+            onChange={
+              this.handleChangeFirstName
+            }
+          />
+        </label>
       </div>
     );
   }
