be5ecf703635f9cbedd7a85d26a6cd9fac6ed0f8
Carl Camit <carlcamit0@gmail.com>
2017-11-29 11:25:55 +1100

    moved data into states instead of being hard-coded

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  state = {
+    user: {
+      firstName: 'Rachel',
+      lastName: 'Jones',
+      profileImageURL: 'https://randomuser.me/api/portraits/women/57.jpg'
+    }
+  }
+
   render() {
+    const user = this.state.user
+
     return (
       <div className="App">
         <h1>LinkedIn Profile Editor</h1>
-        <img src="https://randomuser.me/api/portraits/women/57.jpg" alt="Rachel Jones"/>
-        <p>Name: Rachel Jones</p>
+        <img src={ user.profileImageURL } alt={ user.firstName + ' ' + user.lastName }/>
+        <p>Name: { user.firstName } { user.lastName } </p>
       </div>
     );
   }
